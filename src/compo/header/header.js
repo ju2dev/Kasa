52fd17b6cc32b3ff3dacff4d2ca4840b5d571354
ju2dev <j.piergiovanni@icloud.com>
2024-09-25 13:02:20 +0200

    modif js header

--- src/compo/header/header.js
+++ src/compo/header/header.js
@@ -4,37 +4,39 @@ import { NavLink } from 'react-router-dom';
 import logo from "../../assets/logo/LOGO.svg";
 
 function Header() {
-    return (
-      <header className="header">
-        <div>
-          <NavLink to="/">
-            <img className="header_logo" src={logo} alt="Logo Kasa"></img>
-          </NavLink>
-        </div>
-        <nav className="header_nav">
-          <ul className="header_nav_">
-            <li>
-              {/* Lien de navigation vers la page d'accueil */}
-              <NavLink
-                activeClassName="active"
-                className="header_list"
-                to="/"
-              >Accueil
-              </NavLink>
-            </li>
-            <li>
-              {/* Lien de navigation vers la page À propos */}
-              <NavLink
-                activeClassName="active" 
-                className="header_list"
-                to="/propos"
-              >A propos
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
+  return (
+    <header className="header">
+      <div>
+        <NavLink to="/">
+          <img className="header_logo" src={logo} alt="Logo Kasa" />
+        </NavLink>
+      </div>
+      <nav className="header_nav">
+        <ul className="header_nav_">
+          <li>
+            <NavLink 
+              className={({ isActive }) => 
+                isActive ? "header_list active" : "header_list"
+              }
+              to="/"
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              className={({ isActive }) => 
+                isActive ? "header_list active" : "header_list"
+              }
+              to="/propos"
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
