6bce97168592438c3904361d599ec4f5eca9aa9d
ju2dev <j.piergiovanni@icloud.com>
2024-09-25 13:02:55 +0200

    modif gallerie js

--- src/compo/Gallerie/gallerie.js
+++ src/compo/Gallerie/gallerie.js
@@ -2,24 +2,22 @@ import React from "react";
 import './gallerie.css'; 
 import { Link } from "react-router-dom";
 import Carte from '../../compo/cartes/carte'; 
-import logements from '../../data/logement.json';
+import logements from '../../data/logement.json'; 
 
 const Gallerie = () => {
     return (
       <div className="fond">
-      <div className="Gallerie">
-        {logements.map((logement) => {
-          return (
-            <article key={logement.id}>
+        <div className="Gallerie">
+          {logements.map((logement) => (
+            <article key={logement.id} className="gallerie-item">
               <Link to={`/logement/${logement.id}`}>
                 <Carte image={logement.cover} title={logement.title} />
               </Link>
             </article>
-          );
-        })}
+          ))}
         </div>
-        </div>
+      </div>
     );
-  };
-  
-  export default Gallerie;
+};
+
+export default Gallerie;
