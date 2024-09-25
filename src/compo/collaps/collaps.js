c32e165500ce414745c65e53349293b647c3d9e1
ju2dev <j.piergiovanni@icloud.com>
2024-09-25 12:58:57 +0200

    collaps js

---
+++ src/compo/collaps/collaps.js
@@ -0,0 +1,44 @@
+import React, { useState, useRef, useEffect } from "react";
+import flecheHaut from '../../assets/flèches/fleche-haut.png';
+import flecheBas from '../../assets/flèches/fleche-bas.png';
+import './collaps.css';
+
+const Collapse = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  // Gestion dynamique de la hauteur maximale pour la transition
+  useEffect(() => {
+    if (contentRef.current) {
+      if (isOpen) {
+        contentRef.current.style.opacity = 1;
+      } else {
+        contentRef.current.style.opacity = 0;
+      }
+    }
+  }, [isOpen]);
+
+  return (
+    <div className="collapse_container">
+      <div className="collapse_title" onClick={() => setIsOpen(!isOpen)}>
+        <h2>{title}</h2>
+        <p>
+          <img
+            src={isOpen ? flecheBas : flecheHaut}
+            alt={isOpen ? "Flèche vers le bas" : "Flèche vers le haut"}
+            className={isOpen ? "arrow-down" : "arrow-up"}
+          />
+        </p>
+      </div>
+      <div
+        ref={contentRef}
+        className={`collapse_content ${isOpen ? "open" : ""}`}
+      >
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
+
